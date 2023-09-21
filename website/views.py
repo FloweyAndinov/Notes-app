3d4e3ad655efc92ae64dc700488d48967531e710
FloweyAndinov <flowey.andinov@gmail.com>
2023-09-21 15:12:28 +0300

    Added Settings page

--- website/views.py
+++ website/views.py
@@ -71,6 +71,27 @@ def load_submitNote():
             dark=result.darkModeEnabled
     return render_template("submitNote.html", user=current_user, user_name=current_user.username, dark=dark)
 
+@login_required
+@views.route('/settings', methods=['GET'])
+def load_settings():
+    dark=False
+    if current_user.is_authenticated:
+        try:
+            result = db.session.execute(db.select(DarkMode).filter_by(user_id=current_user.id)).scalar_one()
+            print(result.darkModeEnabled)
+        except Exception as e:
+            new_darkmode = DarkMode(user_id=current_user.id)
+            db.session.add(new_darkmode)
+            db.session.commit()
+            result = new_darkmode
+        finally:
+            dark=result.darkModeEnabled
+    return render_template("settings.html", user=current_user, user_name=current_user.username, dark=dark)
+
+
+
+
+
 @views.route('/darkmode', methods=['GET'])
 def darkModeSwitch():
     if current_user.is_authenticated:
